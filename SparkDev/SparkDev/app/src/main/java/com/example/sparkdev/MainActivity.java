4a70031ea9e109b07f61f0967308a0f64f92dba1
Bilal Ali <bilal98ali@gmail.com>
2020-09-23 19:08:19 -0400

    Task #1 (Partially #2) - Bilal Ali

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/MainActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.sparkdev;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+
+    public MainActivity() {
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Howdy!"); // Title.
     }
 }
