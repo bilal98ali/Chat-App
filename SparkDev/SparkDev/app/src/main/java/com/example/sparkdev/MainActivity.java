be0685a383e2e7ce345910d07ddfcbb2e9d3d22f
Ricardo Su <ricardolimasuarez@gmail.com>
2020-10-05 18:34:15 -0400

    Ricardo 15

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/MainActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.sparkdev;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,7 +27,10 @@ public class MainActivity extends AppCompatActivity
     private ViewPager myViewPager;
     private TabLayout myTabLayout;
     private TabsAccessorAdapter myTabsAccessorAdapter;
+
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     public MainActivity() {
     }
@@ -27,16 +41,23 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Howdy!"); // Title.
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        RootRef = FirebaseDatabase.getInstance().getReference();
 
-        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("SparkDev"); // Title.
+
+        myViewPager = findViewById(R.id.main_tabs_pager);
         myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
         myViewPager.setAdapter(myTabsAccessorAdapter);
 
-        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout = findViewById(R.id.main_tabs);
         myTabLayout.setupWithViewPager(myViewPager);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
 
 
@@ -48,14 +69,94 @@ public class MainActivity extends AppCompatActivity
         if(currentUser == null){
             SendUserToLoginActivity();
         }
+        else{
+            VerifyUserExistance();
+        }
+    }
+
+    private void VerifyUserExistance() {
+        String currentUserID = mAuth.getCurrentUser().getUid();
+
+        RootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if((dataSnapshot.child("name").exists())){
+                    Toast.makeText( MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                   SendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+    //MenuInflater inflater = getMenuInflater();
+    //inflater.inflate(R.menu.options_menu,menu);
+    //return true;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+
+    // @Override
+    // public boolean onOptionsItemSelected(MenuItem item)
+    //{
+    //  switch (item.getItemId()) {
+    //    case R.id.main_logout_option:
+    //      mAuth.signOut();
+    //    SendUserToLoginActivity();
+    //  return true;
+    //default:
+    //  return super.onOptionsItemSelected(item);
+    //}
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_logout_option)
+        {
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if (item.getItemId() == R.id.main_settings_option) {
+            SendUserToSettingsActivity();
+        }
+        if (item.getItemId() == R.id.main_find_friends_option) {
+        }
+
+        return true;
     }
 
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LogInActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+        finish();
     }
 
-
-
+    private void SendUserToSettingsActivity() {
+        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(settingsIntent);
+        finish();
+    }
 
 }
