f1460160862da655366e643da211fec7891aec30
Daniela Agueros <viodani01@gmail.com>
2020-10-09 15:01:42 -0400

    Almost done! Just needs task 18 :D

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/GroupChatActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/GroupChatActivity.java
@@ -1,205 +1,29 @@
-
 package com.example.sparkdev;
 
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Iterator;
-
-//import android.support.v7.app.AppCompatActivity;
-
-public class GroupChatActivity extends AppCompatActivity {
-
-    private Toolbar mToolbar;
-    private ImageButton SendMessageButton;
-    private EditText userMessageInput;
-    private ScrollView mScrollView;
-    private TextView displayTextMessages;
-
-    private FirebaseAuth mAuth;
-    private DatabaseReference UsersRef, GroupNameRef, GroupMessageKeyRef;
-
-    private String currentGroupName, currentUserID, currentUserName, currentDate, currentTime;
+public class GroupChatActivity {
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_chat);
-
-        currentGroupName = getIntent().getExtras().get("groupName").toString(); // currentGroupName must be above firebase references
-        Toast.makeText(GroupChatActivity.this,currentGroupName,Toast.LENGTH_SHORT).show();
-
-        mAuth = FirebaseAuth.getInstance();
-        currentUserID = mAuth.getCurrentUser().getUid();
-        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
-        GroupNameRef = FirebaseDatabase.getInstance().getReference().child("Groups").child(currentGroupName);
 
 
-        InitializeFields();
 
-        GetUserInfo();
-
-        SendMessageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SaveMessageInfoToDatabase();
-
-                userMessageInput.setText("");
-
-                // TASK 22 - START - ALI
-                mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
-                // TASK 22 - END - ALI
-            }
-        });
+    public class GroupChatActivity extends AppCompatActivity{
+         private String currentGroupName; // add this variable
     }
 
+    protected void onCreate (Bundle savedInstanceState){
+       // super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.activity_group_chat);  <-- currentGroupName goes below these two
 
-    // TASK 21 - START OF STEP 1 - ALI
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-
-        GroupNameRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
-            {
-                if (dataSnapshot.exists())
-                {
-                    DisplayMessages(dataSnapshot);
-                }
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
-            {
-                if(dataSnapshot.exists())
-                {
-                    DisplayMessages(dataSnapshot);
-                }
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
-    // TASK 21 - END OF STEP 1 - ALI
-
-
-    private void InitializeFields() {
-
-        mToolbar = (Toolbar) findViewById(R.id.group_chat_bar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle(currentGroupName);
-
-        SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
-        userMessageInput = (EditText) findViewById(R.id.input_group_message);
-        displayTextMessages = (TextView) findViewById(R.id.group_chat_text_display);
-        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
+        currentGroupName = getIntent().getExtras.get("groupName").toString();
+        Toast.makeText(GroupChatActivity.this, currentGroupName, Toast.LENGTH_SHORT).show();
     }
 
-    private void GetUserInfo() {
-        UsersRef.child(currentUserID).addValueEventListener((new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    currentUserName = dataSnapshot.child("name").getValue().toString();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        }));
+    private void InitializeFields(){
+        getSupportActionBar().setTitle(currentGroupName); // this replaces original third line of InitializeFields() method
     }
-
-    private void SaveMessageInfoToDatabase() {
-        String message = userMessageInput.getText().toString();
-        String messageKEY  = GroupNameRef.push().getKey();
-        // ^this is the spelling he used in the video, so I kept it like that even tho its ugly -Dani
-
-        if(TextUtils.isEmpty(message)){
-            Toast.makeText(this, "Please write message first...", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Calendar calForDate = Calendar.getInstance();
-            SimpleDateFormat currentDateFormat = new SimpleDateFormat("MMM dd, yyyy");
-            currentDate = currentDateFormat.format(calForDate.getTime());
-
-            Calendar calForTime = Calendar.getInstance();
-            SimpleDateFormat currentTimeFormat = new SimpleDateFormat("hh:mm a");
-            currentTime = currentTimeFormat.format(calForTime.getTime());
-
-            HashMap<String, Object> groupMessageKey = new HashMap<>();
-            GroupNameRef.updateChildren(groupMessageKey);
-
-            GroupMessageKeyRef = GroupNameRef.child(messageKEY);
-
-            HashMap<String, Object> messageInfoMap = new HashMap<>();
-                messageInfoMap.put("name", currentUserName);
-                messageInfoMap.put("message", message);
-                messageInfoMap.put("date", currentDate);
-                messageInfoMap.put("time", currentTime);
-             GroupMessageKeyRef.updateChildren(messageInfoMap);
-        }
-    }
-
-
-    // TASK 21 - START OF STEP 2 - ALI
-    private void DisplayMessages(DataSnapshot dataSnapshot)
-    {
-        Iterator iterator = dataSnapshot.getChildren().iterator();
-
-        while(iterator.hasNext())
-        {
-            String chatDate = (String) ((DataSnapshot)iterator.next()).getValue();
-            String chatMessage = (String) ((DataSnapshot)iterator.next()).getValue();
-            String chatName = (String) ((DataSnapshot)iterator.next()).getValue();
-            String chatTime = (String) ((DataSnapshot)iterator.next()).getValue();
-
-            displayTextMessages.append(chatName + " :\n" + chatMessage + "\n" + chatTime + "     " + chatDate + "\n\n\n");
-            // The code above deals with the formatting of the messages, when displayed.
-
-            mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
-
-        }
-    }
-    // TASK 21 - END OF STEP 2 - ALI
-
 }
