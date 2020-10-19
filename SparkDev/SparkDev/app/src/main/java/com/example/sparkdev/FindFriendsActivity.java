1045b212b174ec32ef2835b7673fbddbac8cdf53
feliasdager89 <felias_89@DESKTOP-F6PS6KH.localdomain>
2020-10-19 16:08:54 -0400

    the app crashes when the find friends option is clicked

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/FindFriendsActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/FindFriendsActivity.java
@@ -3,11 +3,23 @@ package com.example.sparkdev;
 //import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 //import android.widget.Toolbar;
 
@@ -16,6 +28,8 @@ public class FindFriendsActivity extends AppCompatActivity {
 
     private Toolbar mToolBar;
     private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UsersRef;
+
 
 
 
@@ -23,6 +37,8 @@ public class FindFriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
         FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
@@ -33,5 +49,54 @@ public class FindFriendsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                        .setQuery(UsersRef, Contacts.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
+
+                        holder.userName.setText(model.getName());
+                        holder.userStatus.setText(model.getStatus());
+                      //  Picasso.get().load(model.getImage()).into(holder.profileImage);
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
+                        FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+    }
+
+    public static class FindFriendViewHolder extends RecyclerView.ViewHolder{
+
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+
+
+        public FindFriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.users_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+        }
     }
 }
