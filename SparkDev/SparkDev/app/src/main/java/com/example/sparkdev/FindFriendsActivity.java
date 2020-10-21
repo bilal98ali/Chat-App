0fbeaab087383875bc880fa997fd8056c2e28686
Daniela Agueros <60014028+daniaa760@users.noreply.github.com>
2020-10-21 19:14:42 -0400

    Task 31 not working - app crashes when you click on friend

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/FindFriendsActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/FindFriendsActivity.java
@@ -2,6 +2,7 @@ package com.example.sparkdev;
 
 //import android.support.v7.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -64,11 +65,23 @@ public class FindFriendsActivity extends AppCompatActivity {
         FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, final int position, @NonNull Contacts model) {
 
                         holder.userName.setText(model.getName());
                         holder.userStatus.setText(model.getStatus());
                         Picasso.get().load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                String visit_user_id = getRef(position).getKey();
+
+                                Intent profileIntent = new Intent(FindFriendsActivity.this, ProfileActivity.class);
+                                profileIntent.putExtra("visit_user_id", visit_user_id);
+                                startActivity(profileIntent);
+                            }
+                        });
                     }
 
                     @NonNull
