ae1e9b00708a81f32a88b6ebccbaadf40567d503
Daniela Agueros <60014028+daniaa760@users.noreply.github.com>
2020-10-23 12:15:03 -0400

    Task 31 works- added profileactivity.java and activity_profile.xml

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/ProfileActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/ProfileActivity.java
@@ -1,15 +1,16 @@
 package com.example.sparkdev;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class ProfileActivity extends AppCompatActivity {
+
     private String receiverUserID;
 
-
-    protected void onCreate(Bundle savedInstanceState){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
@@ -18,4 +19,3 @@ public class ProfileActivity extends AppCompatActivity {
         Toast.makeText(this, "User ID: " + receiverUserID, Toast.LENGTH_SHORT).show();
     }
 }
-
