2e7074a7dbcbc16ff397e12e23ff2ef729757fba
Michael Duboc <47266451+MikeDub1@users.noreply.github.com>
2020-11-18 21:54:44 -0500

    Read the branch title dummy!

--- SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/RegisterActivity.java
+++ SparkDev/SparkDev/app/src/main/java/com/example/sparkdev/RegisterActivity.java
@@ -34,7 +34,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_register_insta);
 
         mAuth = FirebaseAuth.getInstance();
         RootRef = FirebaseDatabase.getInstance().getReference();
